45f81f34b9c3eb811ea035a8498da9f599b9af96
wickwirez-beep <wickwirez-beep@users.noreply.github.com>
2026-07-25 21:20:56 -0500

    Fix jvmTarget compilerOptions DSL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,12 +27,14 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
     buildFeatures {
         compose = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
