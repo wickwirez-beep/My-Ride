40e2425af47189b93fba40d75898bd8d68ebb406
wickwirez-beep <wickwirez-beep@users.noreply.github.com>
2026-07-29 23:21:56 -0500

    Add missing imports for Save Specs button

--- app/src/main/java/com/wickwirez/myride/ui/VehicleSpecsScreen.kt
+++ app/src/main/java/com/wickwirez/myride/ui/VehicleSpecsScreen.kt
@@ -18,6 +18,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import com.wickwirez.myride.R
 import com.wickwirez.myride.model.Vehicle
 
